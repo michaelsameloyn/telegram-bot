ef6886b084417b9fb6424458da0ebbb83eaeab47
michaelsameloyn <michaelsameloyn@gmail.com>
2026-06-24 08:28:11 +0300

    Update leads.py

--- services/leads.py
+++ services/leads.py
@@ -11,78 +11,86 @@ def add_lead(
     created_at
 ):
 
-    cur.execute("""
-
-    INSERT OR REPLACE INTO leads(
-
-        telegram_id,
-
-        username,
-
-        full_name,
-
-        referral_code,
-
-        referral_name,
-
-        discount,
-
-        created_at
-
+    cur.execute(
+        """
+        SELECT telegram_id
+        FROM leads
+        WHERE telegram_id=?
+        """,
+        (telegram_id,)
     )
 
-    VALUES(
+    exists = cur.fetchone()
 
-        ?,?,?,?,?,?,?
+    if exists:
 
-    )
+        cur.execute(
+            """
+            UPDATE leads
+            SET
+                username=?,
+                full_name=?
+            WHERE telegram_id=?
+            """,
+            (
+                username,
+                full_name,
+                telegram_id
+            )
+        )
 
-    """, (
+    else:
 
-        telegram_id,
-
-        username,
-
-        full_name,
-
-        referral_code,
-
-        referral_name,
-
-        discount,
-
-        created_at
-
-    ))
+        cur.execute(
+            """
+            INSERT INTO leads(
+                telegram_id,
+                username,
+                full_name,
+                referral_code,
+                referral_name,
+                discount,
+                created_at
+            )
+            VALUES(?,?,?,?,?,?,?)
+            """,
+            (
+                telegram_id,
+                username,
+                full_name,
+                referral_code,
+                referral_name,
+                discount,
+                created_at
+            )
+        )
 
     conn.commit()
 
 
 def get_all_leads():
 
-    cur.execute("""
-
-    SELECT *
-
-    FROM leads
-
-    ORDER BY id DESC
-
-    """)
+    cur.execute(
+        """
+        SELECT *
+        FROM leads
+        ORDER BY id DESC
+        """
+    )
 
     return cur.fetchall()
 
+
 def get_bonus_balance(user_id):
 
-    cur.execute("""
-
-    SELECT bonuses
-
-    FROM leads
-
-    WHERE telegram_id=?
-
-    """, (user_id,))
+    cur.execute(
+        """
+        SELECT bonuses
+        FROM leads
+        WHERE telegram_id=?
+        """,
+        (user_id,)
+    )
 
     row = cur.fetchone()
 
@@ -92,53 +100,52 @@ def get_bonus_balance(user_id):
     return 0
 
 
-def become_partner(
-    user_id,
-    code
-):
+def become_partner(user_id, code):
 
-    cur.execute("""
-
-    UPDATE leads
-
-    SET
-
-        is_partner=1,
-
-        personal_ref_code=?,
-
-        referral_percent=10
-
-    WHERE telegram_id=?
-
-    """, (
-
-        code,
-        user_id
-
-    ))
+    cur.execute(
+        """
+        UPDATE leads
+        SET
+            is_partner=1,
+            personal_ref_code=?,
+            referral_percent=10
+        WHERE telegram_id=?
+        """,
+        (
+            code,
+            user_id
+        )
+    )
 
     conn.commit()
 
 
-def add_bonus(
-    user_id,
-    amount
-):
+def add_bonus(user_id, amount):
 
-    cur.execute("""
-
-    UPDATE leads
-
-    SET bonuses = bonuses + ?
-
-    WHERE telegram_id=?
-
-    """, (
-
-        amount,
-        user_id
-
-    ))
+    cur.execute(
+        """
+        UPDATE leads
+        SET bonuses = bonuses + ?
+        WHERE telegram_id=?
+        """,
+        (
+            amount,
+            user_id
+        )
+    )
 
     conn.commit()
+
+
+def get_partner_by_code(code):
+
+    cur.execute(
+        """
+        SELECT *
+        FROM leads
+        WHERE personal_ref_code=?
+        """,
+        (code,)
+    )
+
+    return cur.fetchone()
