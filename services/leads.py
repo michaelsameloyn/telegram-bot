0e6a7232b33dde4ef68a027b712f9bde86ca20ae
michaelsameloyn <michaelsameloyn@gmail.com>
2026-06-24 06:57:28 +0300

    Update leads.py

--- services/leads.py
+++ services/leads.py
@@ -71,3 +71,74 @@ def get_all_leads():
     """)
 
     return cur.fetchall()
+
+def get_bonus_balance(user_id):
+
+    cur.execute("""
+
+    SELECT bonuses
+
+    FROM leads
+
+    WHERE telegram_id=?
+
+    """, (user_id,))
+
+    row = cur.fetchone()
+
+    if row:
+        return row["bonuses"]
+
+    return 0
+
+
+def become_partner(
+    user_id,
+    code
+):
+
+    cur.execute("""
+
+    UPDATE leads
+
+    SET
+
+        is_partner=1,
+
+        personal_ref_code=?,
+
+        referral_percent=10
+
+    WHERE telegram_id=?
+
+    """, (
+
+        code,
+        user_id
+
+    ))
+
+    conn.commit()
+
+
+def add_bonus(
+    user_id,
+    amount
+):
+
+    cur.execute("""
+
+    UPDATE leads
+
+    SET bonuses = bonuses + ?
+
+    WHERE telegram_id=?
+
+    """, (
+
+        amount,
+        user_id
+
+    ))
+
+    conn.commit()
